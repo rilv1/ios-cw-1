2f267b079661691c6c862601161fd8e4257be34d
فهد فهيد فهد - C53 <alsubaeih5@gmail.com>
2020-09-09 20:14:11 +0300

    done

---
+++  CW-1-1/ CW-1-1.playground/Contents.swift
@@ -0,0 +1,25 @@
+
+let name = "فهد"
+var height = 172
+var age = 17
+var hobby1 = "السباحه"
+var hobby2 = "كره القدم"
+
+print("اسمي \(name)\(age) هواياتي \(hobby1) \(hobby2)")
+
+var firendName = "سعود"
+
+var friendHeight = 170
+
+var friendAge = 17
+
+var friendHobby1 = "كره الطائره"
+
+var friendHobby2 = "كره القدم"
+
+var age_difference = age - friendAge
+
+age>friendAge
+
+var diffrence = hobby1 == friendHobby2 || hobby2 == friendHobby1 || hobby1 == friendHobby1 || hobby2 == friendHobby2
+
